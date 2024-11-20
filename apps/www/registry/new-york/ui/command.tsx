0e2774bd255be6add68afefc1519ac1a8d3478eb
Nicola Dal Maso (u20939) <nicola.dalmaso@gpi.it>
2024-11-20 20:54:09 +0100

    draft

--- apps/www/registry/new-york/ui/command.tsx
+++ apps/www/registry/new-york/ui/command.tsx
@@ -6,7 +6,7 @@ import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Dialog, DialogContent } from "@/registry/new-york/ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "@/registry/new-york/ui/dialog"
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -23,10 +23,18 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-const CommandDialog = ({ children, ...props }: DialogProps) => {
+
+type CommandDialogProps = DialogProps & {
+  accessibleTitle: string;
+  accessibleDescription?: string;
+}
+
+const CommandDialog = ({ children, accessibleTitle, accessibleDescription, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0">
+        <DialogTitle className="sr-only">{accessibleTitle}</DialogTitle>
+        <DialogDescription className="sr-only">{accessibleDescription}</DialogDescription>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
